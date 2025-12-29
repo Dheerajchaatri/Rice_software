ef4ba236e5d25e9ea4be8ea11a99575e782f6c39
Dheeraj Kumar <120976659+Dheerajchaatri@users.noreply.github.com>
2025-12-29 22:24:44 +0500

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,1 +1,31 @@
+from database import get_db
+
+def update_inventory(variety, qty, rate):
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT quantity, avg_cost FROM inventory WHERE variety=?",
+        (variety,)
+    )
+    row = cur.fetchone()
+
+    if row:
+        old_qty, old_cost = row
+        new_qty = old_qty + qty
+        new_cost = ((old_qty * old_cost) + (qty * rate)) / new_qty
+
+        cur.execute("""
+        UPDATE inventory
+        SET quantity=?, avg_cost=?
+        WHERE variety=?
+        """, (new_qty, new_cost, variety))
+    else:
+        cur.execute("""
+        INSERT INTO inventory (variety, quantity, avg_cost)
+        VALUES (?, ?, ?)
+        """, (variety, qty, rate))
+
+    conn.commit()
+    conn.close()
 
