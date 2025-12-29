adcc2deaddb2f26d125edfba729261dea5a8df03
Dheeraj Kumar <120976659+Dheerajchaatri@users.noreply.github.com>
2025-12-29 22:24:08 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,22 @@
+import sqlite3
+
+def get_db():
+    conn = sqlite3.connect("rice.db")
+    return conn
+
+def setup_db():
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS inventory (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        variety TEXT,
+        quantity REAL,
+        avg_cost REAL
+    )
+    """)
+
+    conn.commit()
+    conn.close()
 
